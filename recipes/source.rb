95f5cc080a361fd67a46b94ad6c8b502e3109264
Eric Saxby <sax@wanelo.com>
2014-05-16 14:06:38 -0700

    paythoarn

--- recipes/source.rb
+++ recipes/source.rb
@@ -12,8 +12,17 @@ execute 'untar rabbitmq' do
   not_if { File.directory?(helper.source_directory) }
 end
 
-execute 'patch rabbitmq source' do
+execute 'patch rabbitmq source : sh' do
   command %{find . -type f | xargs sed -i 's^#!\/bin\/sh^#!\/usr\/xpg4\/bin\/sh^'}
+  cwd helper.source_directory
+  only_if { helper.requires_sh_patching? }
+end
+
+execute 'patch rabbitmq source : python' do
+  command %{
+    find . -type f | xargs sed -i 's^#!\/usr\/bin\/python^#!\/opt\/local\/bin\/python^'
+    find . -type f | xargs sed -i 's^#!\/usr\/bin\/env python^#!\/opt\/local\/bin\/python^'
+  }
   cwd helper.source_directory
   only_if { helper.requires_sh_patching? }
 end
