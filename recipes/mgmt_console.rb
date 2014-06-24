e1a6beb0b937432fbe10c325d1f37c449f2c9679
Eric Saxby <sax@wanelo.com>
2014-06-23 17:56:11 -0700

    Add rabbitmq::mgmt_console recipe
    Trying to match the recipe expectations from the
    Opscode rabbitmq cookbook.

---
+++ recipes/mgmt_console.rb
@@ -0,0 +1,9 @@
+%w(
+  rabbitmq_management
+  rabbitmq_management_visualiser
+).each do |plugin|
+  rabbitmq_plugin plugin do
+    action :enable
+    notifies :restart, "service[rabbitmq]"
+  end
+end
