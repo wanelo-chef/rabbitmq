a6de1c752a1b9b20f5907abce07258efe2c65077
James Hart <james@wanelo.com>
2016-11-04 13:51:10 -0700

    Set HOME environment variable

--- libraries/rabbit_mq.rb
+++ libraries/rabbit_mq.rb
@@ -1,20 +1,23 @@
 module RabbitMQ
+  # TODO: Make configurable
+  DEFAULT_OPTIONS = { env: { 'HOME' => '/root' } }
+
   def self.ctl(cmd, options = {})
-    cmd = Mixlib::ShellOut.new("su - rabbitmq -c 'rabbitmqctl -q #{cmd}'", options)
+    cmd = Mixlib::ShellOut.new("su - rabbitmq -c 'rabbitmqctl -q #{cmd}'", DEFAULT_OPTIONS.merge(options))
     cmd.run_command
     cmd.error!
     cmd
   end
 
   def self.plugins(cmd, options = {})
-    cmd = Mixlib::ShellOut.new("rabbitmq-plugins #{cmd}", options)
+    cmd = Mixlib::ShellOut.new("rabbitmq-plugins #{cmd}", DEFAULT_OPTIONS.merge(options))
     cmd.run_command
     cmd.error!
     cmd
   end
 
   def self.admin(cmd, options = {})
-    cmd = Mixlib::ShellOut.new("rabbitmqadmin #{cmd}", options)
+    cmd = Mixlib::ShellOut.new("rabbitmqadmin #{cmd}", DEFAULT_OPTIONS.merge(options))
     cmd.run_command
     cmd.error!
     cmd
--- spec/libraries/rabbit_mq_spec.rb
+++ spec/libraries/rabbit_mq_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'rabbit_mq'
 
 describe RabbitMQ do
+  let(:defaults) { RabbitMQ::DEFAULT_OPTIONS }
   let(:shellout) { double(run_command: true, error!: true) }
 
   before do
@@ -12,7 +13,7 @@ describe RabbitMQ do
     it 'shells out to rabbitmqctl as the rabbitmq user' do
       cmd = 'stuff with things'
       RabbitMQ.ctl(cmd)
-      expect(Mixlib::ShellOut).to have_received(:new).with("su - rabbitmq -c 'rabbitmqctl -q #{cmd}'", {})
+      expect(Mixlib::ShellOut).to have_received(:new).with("su - rabbitmq -c 'rabbitmqctl -q #{cmd}'", defaults)
     end
   end
 
@@ -20,7 +21,7 @@ describe RabbitMQ do
     it 'shells out to rabbitmq-plugins' do
       cmd = 'stuff with things'
       RabbitMQ.plugins(cmd)
-      expect(Mixlib::ShellOut).to have_received(:new).with("rabbitmq-plugins #{cmd}", {})
+      expect(Mixlib::ShellOut).to have_received(:new).with("rabbitmq-plugins #{cmd}", defaults)
     end
   end
 
@@ -28,7 +29,7 @@ describe RabbitMQ do
     it 'shells out to rabbitmqadmin' do
       cmd = 'check some stuff'
       RabbitMQ.admin(cmd)
-      expect(Mixlib::ShellOut).to have_received(:new).with("rabbitmqadmin #{cmd}", {})
+      expect(Mixlib::ShellOut).to have_received(:new).with("rabbitmqadmin #{cmd}", defaults)
     end
   end
 end
